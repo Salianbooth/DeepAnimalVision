20442b9a6b2d534bbb84085530c6ebf40a3a9585
Salianbooth <czc434579527@163.com>
2026-01-29 18:17:24 +0800

    [feat] 新增登录界面，添加路由，整理前端项目

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,8 +2,10 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import { createPinia } from 'pinia'
+import router from './router'
 
 const app = createApp(App)
 app.use(createPinia())
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
